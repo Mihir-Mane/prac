b056566370c9c546eae14299132aca98c75660aa
Mihir Mane <70523371+Mihir-Mane@users.noreply.github.com>
2022-12-06 23:29:39 +0530

    Update serverPrime.java

--- CloudComputingPractical/Client Server communication model/serverPrime.java
+++ CloudComputingPractical/Client Server communication model/serverPrime.java
@@ -1,25 +1,24 @@
 import java.net.*;
 import java.io.*;
-class serverPrime{
+class serverPrime {
     public static void main(String args[]){
         try{
-            ServerSocket ss = new ServerSocket(8001);
-            System.out.println("Server Started...............");
-            Socket s = ss.accept();
-            DataInputStream in = new
-            DataInputStream(s.getInputStream()); int x= in.readInt();
-            DataOutputStream otc = new
-            DataOutputStream(s.getOutputStream()); int y = x/2;
-            if(x ==1 || x ==2 || x ==3){
-                otc.writeUTF(x + "is Prime");
+            ServerSocket socket = new ServerSocket(8001);
+            System.out.println("Server Strated.");
+            Socket soc = socket.accept();
+            DataInputStream inp = new DataInputStream(soc.getInputStream());
+            int x = inp.readInt();
+            DataOutputStream out = new DataOutputStream(soc.getOutputStream());
+            int y = x / 2;
+            if(x==1 || x==2 || x==3){
+                out.writeUTF(x+" is prime");
                 System.exit(0);
             }
             for(int i=2; i<=y; i++){
                 if(x%i != 0){
-                    otc.writeUTF(x + " is Prime");
-                }
-                else{
-                    otc.writeUTF(x + " is not Prime");
+                    out.writeUTF(x+" is prime.");
+                } else{
+                    out.writeUTF(x+" is not prime");
                 }
             }
         }
